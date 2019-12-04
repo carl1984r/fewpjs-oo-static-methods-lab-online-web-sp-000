37168682e1994fc5430a0c4349ffd9f27c862ecb
Bry Cox <carl.cc@gmail.com>
2019-12-04 07:27:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ class Formatter {
   static capitalize(str) {
     return str[0].toUpperCase() + str.slice(1);
   };
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]/g,  '');
+  };
+  
 }
