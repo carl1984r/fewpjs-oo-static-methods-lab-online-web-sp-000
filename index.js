f8e0c061b9f3a761da2a88996ead9a6edd52ac34
Bry Cox <carl.cc@gmail.com>
2019-12-04 07:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ class Formatter {
     return str.replace(/[^A-Za-z0-9-' ]/g,  '');
   };
 
+  static titleize(string) {
+  const titleizedString = [];
+  const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  const splitString = string.split(' ');
+  titleizedString.push(this.capitalize(splitString[0]));
+  for(const word of splitString.slice(1)) {
+    if(!exceptions.find(exception => exception === word)) { titleizedString.push(this.capitalize(word)) } else { titleizedString.push(word) };
+  };
+  return titleizedString.join(' ');
+}
+
 }
